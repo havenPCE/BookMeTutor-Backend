9a01d028bbf7d1636e0e118ca8615ff470589dff
shyam9583 <strikerallin1@gmail.com>
2020-03-19 14:34:27 +0530

    Added delete endpoint for subject

--- src/main/java/com/pce/BookMeTutor/Controllers/SubjectController.java
+++ src/main/java/com/pce/BookMeTutor/Controllers/SubjectController.java
@@ -7,7 +7,9 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pce.BookMeTutor.Config.Constants;
 import com.pce.BookMeTutor.Model.Dao.Subject;
 import com.pce.BookMeTutor.Model.Dto.Requests.SubjectDTO;
+import com.pce.BookMeTutor.Model.Dto.Responses.MessageResponse;
 import com.pce.BookMeTutor.Repo.SubjectRepo;
 
 @RestController
@@ -61,6 +64,15 @@ public class SubjectController {
 
 		return ResponseEntity.ok(subjectRepo.save(subject));
 
+	}
+	
+	@DeleteMapping("/subject/{id}")
+	public ResponseEntity<?> deleteSubject(@PathVariable("id") long id) {
+		Subject subject = subjectRepo.findById(id).get();
+		if(subject == null)
+			return new ResponseEntity<>(Constants.SUBJECT_NOT_FOUND, HttpStatus.NOT_FOUND);
+		subjectRepo.delete(subject);
+		return ResponseEntity.ok(new MessageResponse("subject["+id+"] removed"));
 	}
 
 }
